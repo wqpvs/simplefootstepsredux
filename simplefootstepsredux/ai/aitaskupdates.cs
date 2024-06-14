58f068b1f52eb142c1d779f1d69d3c59f29b8613
Quentin Preik <quentin@preik.net>
2024-06-14 05:57:22 -0400

    Add project files.

---
+++ simplefootstepsredux/ai/aitaskupdates.cs
@@ -0,0 +1,247 @@
+﻿using Vintagestory.API.Common;
+using Vintagestory.GameContent;
+
+namespace simplefootstepsredux
+{
+
+    public class AiTaskLoudDrifterWander : AiTaskWander
+    {
+
+        public AssetLocation stepSound = new AssetLocation("simplestep", "sounds/creature/steps/drifter");
+        public float stepTimer = new float();
+        
+
+        public override bool ContinueExecute(float dt)
+        {
+            stepTimer += dt;
+
+            if (stepTimer >= 0.84)
+            {
+                world.PlaySoundAt(stepSound, entity.Pos.X, entity.Pos.Y, entity.Pos.Z,null,true,20f,0.5f);
+                stepTimer = 0;
+            }
+
+            return base.ContinueExecute(dt); ;
+
+        }
+        public AiTaskLoudDrifterWander(EntityAgent entity) : base(entity)
+        {
+
+        }
+    }
+
+    public class AiTaskLoudDrifterSeekEntity : AiTaskSeekEntity
+    {
+
+        public AssetLocation stepSound = new AssetLocation("simplestep", "sounds/creature/steps/drifter");
+        public float stepTimer = new float();
+
+
+        public override bool ContinueExecute(float dt)
+        {
+            stepTimer += dt;
+
+            if (stepTimer >= 0.4)
+            {
+                world.PlaySoundAt(stepSound, entity.Pos.X, entity.Pos.Y, entity.Pos.Z, null, true, 20f, 0.5f);
+                stepTimer = 0;
+            }
+
+            return base.ContinueExecute(dt); ;
+
+        }
+        public AiTaskLoudDrifterSeekEntity(EntityAgent entity) : base(entity)
+        {
+
+        }
+    }
+
+    public class AiTaskLoudDrifterFleeEntity : AiTaskFleeEntity
+    {
+
+        public AssetLocation stepSound = new AssetLocation("simplestep", "sounds/creature/steps/drifter");
+        public float stepTimer = new float();
+
+
+        public override bool ContinueExecute(float dt)
+        {
+            stepTimer += dt;
+
+            if (stepTimer >= 0.4)
+            {
+                world.PlaySoundAt(stepSound, entity.Pos.X, entity.Pos.Y, entity.Pos.Z, null, true, 20f, 0.5f);
+                stepTimer = 0;
+            }
+
+            return base.ContinueExecute(dt); ;
+
+        }
+        public AiTaskLoudDrifterFleeEntity(EntityAgent entity) : base(entity)
+        {
+
+        }
+    }
+
+    public class AiTaskLoudDrifterGetOutOfWater : AiTaskGetOutOfWater
+    {
+
+        public AssetLocation stepSound = new AssetLocation("simplestep", "sounds/creature/steps/water");
+        public float stepTimer = new float();
+        
+
+        public override bool ContinueExecute(float dt)
+        {
+            stepTimer += dt;
+
+            if (stepTimer >= 0.84)
+            {
+                world.PlaySoundAt(stepSound, entity.Pos.X, entity.Pos.Y, entity.Pos.Z,null,true,20f,0.5f);
+                stepTimer = 0;
+            }
+
+            return base.ContinueExecute(dt); ;
+
+        }
+        public AiTaskLoudDrifterGetOutOfWater(EntityAgent entity) : base(entity)
+        {
+
+        }
+    }
+
+    
+
+
+
+
+
+    public class AiTaskLoudWander : AiTaskWander
+    {
+        public AssetLocation stepSound = new AssetLocation("simplestep", "sounds/creature/steps/npc");
+        public float stepTimer = new float();
+
+        public override bool ContinueExecute(float dt)
+        {
+            stepTimer += dt;
+
+            if (stepTimer >= 0.55)
+            {
+                world.PlaySoundAt(stepSound, entity.Pos.X, entity.Pos.Y, entity.Pos.Z);
+                stepTimer = 0;
+            }
+
+            return base.ContinueExecute(dt); ; 
+
+        }
+
+        public AiTaskLoudWander(EntityAgent entity) : base(entity)
+        {
+
+        }
+    }
+
+    public class AiTaskLoudFleeEntity : AiTaskFleeEntity
+    {
+        public AssetLocation stepSound = new AssetLocation("simplestep", "sounds/creature/steps/npc");
+        public float stepTimer = new float();
+        public override bool ContinueExecute(float dt)
+        {
+            stepTimer += dt;
+
+            if (stepTimer >= 0.3)
+            {
+                world.PlaySoundAt(stepSound, entity.Pos.X, entity.Pos.Y, entity.Pos.Z);
+                stepTimer = 0;
+            }
+
+            return base.ContinueExecute(dt); ;
+
+        }
+        public AiTaskLoudFleeEntity(EntityAgent entity) : base(entity)
+        {
+
+        }
+    }
+
+    public class AiTaskLoudSeekEntity : AiTaskSeekEntity
+    {
+        public AssetLocation stepSound = new AssetLocation("simplestep", "sounds/creature/steps/npc");
+        public float stepTimer = new float();
+        public override bool ContinueExecute(float dt)
+        {
+            stepTimer += dt;
+
+            if (stepTimer >= 0.3)
+            {
+                world.PlaySoundAt(stepSound, entity.Pos.X, entity.Pos.Y, entity.Pos.Z);
+                stepTimer = 0;
+            }
+
+            return base.ContinueExecute(dt); ;
+
+        }
+        public AiTaskLoudSeekEntity(EntityAgent entity) : base(entity)
+        {
+
+        }
+    }
+
+    public class AiTaskLoudGetOutOfWater : AiTaskGetOutOfWater
+    {
+        public AssetLocation stepSound = new AssetLocation("simplestep", "sounds/creature/steps/water");
+        public float stepTimer = new float();
+
+        public override bool ContinueExecute(float dt)
+        {
+            stepTimer += dt;
+
+            if (stepTimer >= 0.55)
+            {
+                world.PlaySoundAt(stepSound, entity.Pos.X, entity.Pos.Y, entity.Pos.Z);
+                stepTimer = 0;
+            }
+
+            return base.ContinueExecute(dt); ; 
+
+        }
+
+        public AiTaskLoudGetOutOfWater(EntityAgent entity) : base(entity)
+        {
+
+        }
+    }
+
+
+
+
+    public class AiTaskSilentWander : AiTaskWander 
+    {
+        public AiTaskSilentWander(EntityAgent entity) : base(entity)
+        {
+
+        }
+    }
+
+    public class AiTaskSilentFleeEntity : AiTaskFleeEntity
+    {
+        public AiTaskSilentFleeEntity(EntityAgent entity) : base(entity)
+        {
+
+        }
+    }
+
+    public class AiTaskSilentSeekEntity : AiTaskSeekEntity
+    {
+        public AiTaskSilentSeekEntity(EntityAgent entity) : base(entity)
+        {
+
+        }
+    }
+
+    public class AiTaskSilentGetOutOfWater : AiTaskGetOutOfWater 
+    {
+        public AiTaskSilentGetOutOfWater(EntityAgent entity) : base(entity)
+        {
+
+        }
+    }
+}
---
+++ simplefootstepsredux/simplefootstepsreduxModSystem.cs
@@ -0,0 +1,38 @@
+﻿
+using Vintagestory.API.Common;
+using Vintagestory.API.Common.Entities;
+using Vintagestory.API.MathTools;
+using Vintagestory.GameContent;
+
+namespace simplefootstepsredux
+{
+    public class simplefootstepsreduxModSystem : ModSystem
+    {
+        public override void Start(ICoreAPI api)
+        {
+            base.Start(api);
+
+            AiTaskRegistry.Register("wander", typeof(AiTaskLoudWander));
+            AiTaskRegistry.Register("fleeentity", typeof(AiTaskLoudFleeEntity));
+            AiTaskRegistry.Register("seekentity", typeof(AiTaskLoudSeekEntity));
+            AiTaskRegistry.Register("getoutofwater", typeof(AiTaskLoudGetOutOfWater));
+
+            // Seperate Tasks for Drifters, since I want a different sounding footstep so players can easily tell them apart
+            // Would be much cleaner and easier if I could pass a variable from the JSON to use a different sound effect
+            // but not sure if thats possible so I'm just hardcoding it here c:
+
+            AiTaskRegistry.Register("drifterwander", typeof(AiTaskLoudDrifterWander));
+            AiTaskRegistry.Register("drifterseekentity", typeof(AiTaskLoudDrifterSeekEntity));
+            AiTaskRegistry.Register("drifterfleeentity", typeof(AiTaskLoudDrifterFleeEntity));
+            AiTaskRegistry.Register("driftergetoutofwater", typeof(AiTaskLoudDrifterGetOutOfWater));
+
+            // Reclassify vanilla aitasks w/ "silent-" prefix for use to override manually
+
+            AiTaskRegistry.Register("silent-wander", typeof(AiTaskSilentWander));
+            AiTaskRegistry.Register("silent-fleeentity", typeof(AiTaskSilentFleeEntity));
+            AiTaskRegistry.Register("silent-seekentity", typeof(AiTaskSilentSeekEntity));
+            AiTaskRegistry.Register("silent-getoutofwater", typeof(AiTaskSilentGetOutOfWater));
+
+        }
+    }
+}
